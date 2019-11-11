bf49550f37b6abb519f5ada5f38dee3d369bfe13
Kritos123 <56917636+Kritos123@users.noreply.github.com>
2019-11-11 19:54:38 +0100

    Add files via upload

---
+++ Weebs_web/Weebs/App_Code/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        // Další informace o konfiguraci aplikace najdete na webu https://go.microsoft.com/fwlink/?LinkId=316888.
+        app.UseCookieAuthentication(new CookieAuthenticationOptions
+        {
+            AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            LoginPath = new PathString("/Login")
+        });
+        
+    }
+}
